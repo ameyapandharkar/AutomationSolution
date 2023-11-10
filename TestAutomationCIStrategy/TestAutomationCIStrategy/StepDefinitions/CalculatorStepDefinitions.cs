53a6d8880006c529a45c37b4981e9d558ff300d3
AMEYA PANDHARKAR <pandharkarameya@gmail.com>
2023-11-10 22:58:55 +0530

    Updated Step Def
    Updated

--- TestAutomationCIStrategy/TestAutomationCIStrategy/StepDefinitions/CalculatorStepDefinitions.cs
+++ TestAutomationCIStrategy/TestAutomationCIStrategy/StepDefinitions/CalculatorStepDefinitions.cs
@@ -3,42 +3,32 @@ namespace TestAutomationCIStrategy.StepDefinitions
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        public int sum;
+        public int Num1;
+        public int Num2;
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            throw new PendingStepException();
+            Num1 = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-
-            throw new PendingStepException();
+            Num2 = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            throw new PendingStepException();
+            sum = Num1 + Num2;
         }
 
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
-            //TODO: implement assert (verification) logic
-
-            throw new PendingStepException();
+            Console.WriteLine(sum);
         }
     }
 }
